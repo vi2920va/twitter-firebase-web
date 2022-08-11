c044ee3740091fb8359aaa5e062c8b2ebfcdece5
이송현 <isonghyeon@isonghyeon-ui-MacBookPro.local>
2022-08-11 17:18:16 +0900

    :heavy_plus_sign: Add - Login Form Input

--- src/routes/Auth.tsx
+++ src/routes/Auth.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+    switch (name) {
+      case "email":
+        setEmail(value);
+        break;
+      case "password":
+        setPassword(value);
+        break;
+    }
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          name="email"
+          onChange={handleInput}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={handleInput}
+          required
+        />
+        <button type="submit">Log In</button>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
