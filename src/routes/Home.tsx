7ef3384f2e4522f70fb1d5c42abad2cdfc52a053
이송현 <isonghyeon@isonghyeon-ui-MacBookPro.local>
2022-08-15 17:34:43 +0900

    :heavy_plus_sign: Add
    Add - Getting the Twitter

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "../firebase";
+
+interface TwittersProps {
+  id: string;
+  createAt?: string;
+  twitter?: string;
+}
 
 const Home = () => {
   const [twitter, setTwitter] = useState("");
+  const [twitters, setTwitters] = useState<TwittersProps[]>([]);
+
+  const fetchTiwtters = async () => {
+    const dbtwitters = await dbService.collection("twitter").get();
+    dbtwitters.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setTwitters((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    fetchTiwtters();
+  }, []);
   const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.currentTarget;
     setTwitter(value);
@@ -16,16 +37,23 @@ const Home = () => {
     setTwitter("");
   };
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        type="text"
-        value={twitter}
-        onChange={handleInput}
-        placeholder="What's on your mind"
-        maxLength={120}
-      />
-      <button type="submit">Twitter</button>
-    </form>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={twitter}
+          onChange={handleInput}
+          placeholder="What's on your mind"
+          maxLength={120}
+        />
+        <button type="submit">Twitter</button>
+      </form>
+      <ul>
+        {twitters.map((twitter) => (
+          <li key={twitter.id}>{twitter.twitter}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 export default Home;
